c8e7b28dd6fe8caa566ad7585aff3919b258373f
Dinis Gostev <svobnick@ya.ru>
2019-03-12 00:49:49 +0300

    add registration method to api requests

--- app/src/main/java/com/svobnick/thisorthat/service/ApiRequests.kt
+++ app/src/main/java/com/svobnick/thisorthat/service/ApiRequests.kt
@@ -1,15 +1,15 @@
 package com.svobnick.thisorthat.service
 
-import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.svobnick.thisorthat.dao.QuestionDao
 import com.svobnick.thisorthat.model.Question
 import org.json.JSONObject
+import java.util.*
 
 fun questionsRequest(questionDao: QuestionDao, unansweredQuestions: () -> Unit, nextQuestionToView: () -> Unit) =
     JsonObjectRequest(
-        Request.Method.GET, " http://dev.thisorthat.ru/api/items/get/20", null,
+        "http://dev.thisorthat.ru/api/items/get/20", null,
         Response.Listener { response ->
             response.keys().forEach { key ->
                 val question = response.get(key) as JSONObject
@@ -29,3 +29,19 @@ fun questionsRequest(questionDao: QuestionDao, unansweredQuestions: () -> Unit, 
             System.err.println(it.message)
             it.printStackTrace()
         })
+
+fun registrationRequest() = {
+    var requestJson = JSONObject()
+    requestJson.put("client", "android")
+    // todo use firebase instance id for registration
+    requestJson.put("unique", UUID.randomUUID().toString())
+    JsonObjectRequest(
+        "http://dev.thisorthat.ru/api/users/add/", requestJson,
+        Response.Listener { response ->
+
+        },
+        Response.ErrorListener {
+            System.err.println(it.message)
+            it.printStackTrace()
+        })
+}

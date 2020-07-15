52e4de7af1e743fa81fef1fb28a4478a739d0c65
Dinis Gostev <svobnick@ya.ru>
2020-07-16 00:01:41 +0300

    handle null networkResponse (probably network not available case)

--- app/src/main/kotlin/com/svobnick/thisorthat/utils/ExceptionUtils.kt
+++ app/src/main/kotlin/com/svobnick/thisorthat/utils/ExceptionUtils.kt
@@ -1,22 +1,35 @@
 package com.svobnick.thisorthat.utils
 
+import android.util.Log
 import com.android.volley.VolleyError
 import org.json.JSONObject
+import java.io.PrintWriter
+import java.io.StringWriter
 import kotlin.reflect.KFunction1
 
 object ExceptionUtils {
+    private val TAG = this::class.java.name
 
     fun handleApiErrorResponse(
         it: VolleyError,
         showError: KFunction1<@ParameterName(name = "errorMsg") String, Unit>
     ) {
         if (it.networkResponse?.statusCode == 500) {
-            // todo use strings.xml
             showError.call("Проблемы с сервером, попробуйте позже")
         } else {
-            val errorJson = JSONObject(String(it.networkResponse.data))
-            val reason = (errorJson["description"] as String)
-            showError.call(reason)
+            if (it.networkResponse != null) {
+                if (it.networkResponse.data != null && it.networkResponse.data.isNotEmpty()) {
+                    val errorJson = JSONObject(String(it.networkResponse.data))
+                    val reason = (errorJson["description"] as String)
+                    showError.call(reason)
+                } else {
+                    val writer = PrintWriter(StringWriter())
+                    RuntimeException().printStackTrace(writer)
+                    Log.e(TAG, "Bad network response: ${it.networkResponse}. Stacktrace: \n " + writer.toString())
+                }
+            } else {
+                showError.call("Проблемы с сетью, попробуйте позже")
+            }
         }
     }
 }
